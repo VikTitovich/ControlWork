21f44099b9431f8423a933f6016a6f1c25c10483
Viktor Titovich <raffityinet@gmail.com>
2023-03-14 09:41:17 +0300

    add ReadInt & ReadString

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿int ReadInt(string argument)
+{
+    Console.Write(argument);
+
+    int number;
+
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Error! Try again.");
+    }
+    return number;
+}
+
+string ReadString(string argument)
+{
+    Console.Write(argument);
+
+    string? line = Console.ReadLine();
+
+    return line!;
+}
